8d096960e28992e8f93a8e577935a361ba2b0912
benjaminlgur <benjaminlgur@yahoo.com>
2017-10-19 23:49:39 -0400

    Add files via upload

--- projectone.cpp
+++ projectone.cpp
@@ -4,5 +4,12 @@
 
 using namespace std;
 
+
 int main(){
+    string wordOne, wordTwo, wordThree, wordFour, wordFive, wordSix, wordSeven, wordEight;
+    ifstream words, sentances;
+    ofstream match, replace, allwords;
+    words.open("words.txt")
+    
+}
 
