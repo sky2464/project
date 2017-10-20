70522d3e48dbb34707b96411a644f0d4a1dd368c
benjaminlgur <benjaminlgur@yahoo.com>
2017-10-20 00:33:33 -0400

    Add files via upload

--- projectone.cpp
+++ projectone.cpp
@@ -4,12 +4,27 @@
 
 using namespace std;
 
+bool wordtest(string word);
+//Precondition: Entering a string from the words file.
+//Postcondition: This fuction will return true if the argument exists.
 
 int main(){
     string wordOne, wordTwo, wordThree, wordFour, wordFive, wordSix, wordSeven, wordEight;
-    ifstream words, sentances;
-    ofstream match, replace, allwords;
-    words.open("words.txt")
-    
+    ifstream words, sentances; //defining input streams
+    ofstream match, replace, allwords; //defining output streams
+    words.open("words.txt");
+    if (words.fail()){ //Error checking.
+        cerr << "Error opening words.txt";
+        return(1);
+    }
+    words >> wordOne >> wordTwo >> wordThree >> wordFour >> wordFive >> wordSeven >> wordEight;
 }
 
+bool wordtest(string word);
+    if(word[0] != ' '){
+        return false;
+    }
+    else {
+    return true
+    }
+}
