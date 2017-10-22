a1ca58f66f8050eeda4649217d042c43803feb99
benjaminlgur <benjaminlgur@yahoo.com>
2017-10-21 23:13:54 -0400

    Problamatic replace
    Problamatic replace version

--- projectone.cpp
+++ projectone.cpp
@@ -13,10 +13,10 @@ bool search(string line, string word);
 
 int main(){
     string wordOne, wordTwo, wordThree, wordFour, wordFive, wordSix, wordSeven, wordEight; 
-    string line, periodFinder;
-    int place, x;
+    string line, lineTwo;
+    int place, x, pos;
     bool firstrun = true;
-    ifstream words, sentences; //defining input streams
+    ifstream words, sentences, matchin; //defining input streams
     ofstream match, replace, allwords; //defining output streams
     words.open("words.txt");
     if (words.fail()){ //Error checking.
@@ -26,20 +26,64 @@ int main(){
     words >> wordOne >> wordTwo >> wordThree >> wordFour >> wordFive >> wordSeven >> wordEight;
     words.close();
     sentences.open("sentences.txt");
+    if (sentences.fail()){
+        cerr << "Error finding/creating sentances.txt" << endl;
+        return (2);
+    }
+    match.open("match.txt");
     if (sentences.fail()){ //Error checking.
         cerr << "Error opening sentences.txt" << endl;
-        return(2);
+        return(3);
     }
-    while (sentences.eof() == false){
+    while (sentences.eof() == false){ //This while loop is the match portion of the project.
         if (firstrun == false){
-            sentences.clear();
+            sentences.ignore(); //clean stream of unwanted whitespace.
         }
         getline(sentences, line, '.');
         firstrun = false;
         if (search(line, wordOne) == true){
-            cout << line << endl;
+            char letter;
+            for (int i = 0; i < line.length(); i++){
+                if(i == 0 && line[0] >= 97 && line[0] <= 122){ //checks if the first letter is lower case.
+                    letter = line[i] - 32;
+                    match << letter; //capitlizes first letter.
+                }
+                else{
+                    match << line[i];
+                }
+            }
+                match << "." << endl;
         }
     }
+    match.close();
+    matchin.open("match.txt");
+    if (matchin.fail()){
+        cerr << "Error opening match.txt" << endl;
+        return (4);
+    }
+    replace.open("replace.txt");
+    if (replace.fail()){
+        cerr << "Error finding/creating replace.txt" << endl;
+        return(5);
+    }
+    firstrun = true;
+    while (matchin.eof() == false){ // This loop does the input into the replace text. 
+        if (firstrun == false){
+            matchin.ignore();
+        }
+        getline(matchin, lineTwo);
+        for (int i = 0; i < lineTwo.length(); i++){ 
+            if (lineTwo[i] == wordOne[0] && lineTwo[i + wordOne.length()] == wordOne[wordOne.length()]){ //Currently this only works if there is not words that are the same lenght that differ in the middle for example cat vs car.
+            replace << wordTwo << " ";
+            i = i + wordOne.length();     
+        }
+            else{
+            replace << lineTwo[i];
+            }
+        }
+        replace << endl;
+    }
+    cout << wordTwo.length() << endl;
 }
 
 bool wordtest(string word){
